a12a561884dcc86fcdffca4726d7aed4c2c3b38a
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-02-03 10:18:06 -0300

    fix: Correções no Js
    fiz mais algumas correções no Js e estilizei o botao de remover a tarefa

--- exercicioJQuery/main.js
+++ exercicioJQuery/main.js
@@ -1,27 +1,27 @@
-$(document).ready(function(e) {
-    $("#addTaskBtn").click(function(e) {
+$(document).ready(function () {
+    $("#addTaskBtn").click(function (e) {
         e.preventDefault();
         const taskText = $("#taskInput").val().trim();
 
         if (taskText !== "") {
             const newTask = $("<li>").text(taskText);
 
-            const removeBtn = $("<button>").text("Remover");
+            const removeBtn = $("<button>").text("Remover").addClass("remove-task");
             newTask.append(removeBtn);
 
-            $("#taskList").append(newTask)
+            $("#taskList").append(newTask);
 
-            $("taskInput").val("");
+            $("#taskInput").val("");
         } else {
-            alert ("Por favor, digite uma tarefa!");
+            alert("Por favor, digite uma tarefa!");
         }
     });
 
-    $("#taskList").on("click", "button", function(e) {
+    $("#taskList").on("click", ".remove-task", function () {
         $(this).parent().remove();
     });
 
-    $("#taskList").on("click", "li", function (e) {
+    $("#taskList").on("click", "li", function () {
         $(this).toggleClass("completed");
     });
 });
