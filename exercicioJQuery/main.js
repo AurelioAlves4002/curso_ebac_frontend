1d10ddfa9a0f4103d053e29feb4c47fb090f7228
AurelioAlves4002 <aurelio.alves2004@gmail.com>
2025-02-03 09:47:29 -0300

    feat: Adicionar tarefa
    Criei uma função usando JQuery para adicionar as tarefas na lista

---
+++ exercicioJQuery/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(e) {
+    $("#addTaskBtn").click(function(e) {
+        const taskText = $("taskInput").val().trim();
+
+        if (taskText !== "") {
+            const newTask = $("<li>").text(taskText);
+
+            const removeBtn = $("<button>").text("Remover");
+            newTask.append(removeBtn);
+
+            $("taskList").append(newTask)
+
+            $("taskInput").val("");
+        } else {
+            alert ("Por favor, digite uma tarefa!");
+        }
+    });
+})
